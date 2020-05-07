29813b00e066ffddcd78df94288c7fbe3ad5ace2
Andrey Kozlovskiy <cfc9209@gmail.com>
2020-05-07 13:36:28 +0300

    Update ip_address.rb

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -1,16 +1,4 @@
 class IPAddress < ApplicationRecord
-  # This class becomes manager. So definitely need to extract all these methods below out of this scope
-
-  def self.create_or_find_by_ip_addr(ip_addr)
-    ip_address = new(ip_addr: ip_addr)
-
-    IPAddressBuilder.(ip_address)
-  end
-
-  def new_record_created_event
-    FetchIPAddressInfoJob.perform_later(self)
-  end
-
   def fetch_info
     IPAddressInforFetcher.(self)
   end
